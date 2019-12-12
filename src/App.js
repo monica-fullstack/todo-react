11f093a7d3025f29bf63a92d94052a420d88f16f
Fullstack <monica@fullstack.ph>
2019-12-12 19:31:20 +0800

    initial commit

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import TodoList from './TodoList';
+
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      items: [],
+      currentItem: {text:'', key:''},
+    }
+  }
+  handleInput = e => {
+    const itemText = e.target.value
+    const currentItem = { text: itemText, key: Date.now() }
+    this.setState({
+      currentItem,
+    })
+  }
+  addItem = (e) => {
+    e.preventDefault()
+    const newItem = this.state.currentItem
+    if (newItem.text !== '') {
+      console.log(newItem)
+      const items = [...this.state.items, newItem]
+      this.setState({
+        items: items,
+        currentItem: { test: '', key: '' },
+      })
+    }
+  }
+  render() {
+    return (
+      <div className="App">
+        <TodoList
+          addItem={this.addItem}
+          inputElement={this.inputElement}
+          handleInput={this.handleInput}
+          currentItem={this.state.currentitem}
+        />
+        <TodoItems entries={this.state.items} />
+      </div>
+    );
+  }
+}
+
+export default App;
